22af26be35c7833854bccefbcf9b23da7a6e500f
QuLin007 <hamiltonwong09@gmail.com>
2025-10-19 01:24:16 +0800

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['SF Pro Display', 'PingFang SC', 'system-ui', 'sans-serif']
+      },
+      backdropBlur: {
+        lg: '18px',
+      }
+    },
+  },
+  plugins: [],
+}
